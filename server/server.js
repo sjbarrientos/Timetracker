11cb01c4798d82487fdf6ac656ad0b091e782dde
sjbarrientos <sjbarrientosb@gmail.com>
2019-09-07 01:01:30 -0500

    product routes configured and implemented the apidoc package

--- server/server.js
+++ server/server.js
@@ -1,25 +1,29 @@
+//Load config consts
 require('./config/config');
 
+//Load required dependeces
 const express = require('express');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+//Configure Express
 const app = express();
-
-// create application/json parser
-var jsonParser = bodyParser.json();
-// create application/x-www-form-urlencoded parser
+// Create application/json parser
+const jsonParser = bodyParser.json();
+// Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
+//  Define public path
+app.use(express.static(__dirname + '/public'))
+    //Import routes
+app.use(require('./routes/index.js'));
 
+//Conect to Mongo DB
 mongoose.connect(process.env.DBString, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
     if (err)
         throw err
-    console.log('DB OK');
-
+    console.log('Database connected susscessfuly');
 
 });
 
-
-
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(process.env.PORT, () => console.log(`Example app listening on port port!`));
+//Start server
+app.listen(process.env.PORT, () => console.log(`Time Tracker Api running at port ${process.env.PORT}`));
